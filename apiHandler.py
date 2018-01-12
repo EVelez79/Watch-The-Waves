1b9bdd1ea9e81ab542d93a2b617e306180ff36f1
Erick Velez <erickvelez7@gmail.com>
2018-01-11 20:07:57 -0800

    New declarations for Twitter Auth Twitter Auth is now declared before any method to reduce redundancies

--- apiHandler.py
+++ apiHandler.py
@@ -1,22 +1,31 @@
 import urllib, json, tweepy, configHandler, datetime
 
+config = configHandler.readConfig()
+spots = config["SpitCastAPI"]["Locations"]
 today = datetime.datetime.today()
 tomorrow = today + datetime.timedelta(days=1)
 
 SPITCAST_URL = "http://api.spitcast.com/api/spot/forecast/"
 URL_PARAMETER = "/?dval="
+twitterConfig = config["TwitterAPI"]
+consumerKey = twitterConfig["ConsumerKey"]
+consumerSecret = twitterConfig["ConsumerSecret"]
+accessToken = twitterConfig["OAuthToken"]
+accessTokenSecret = twitterConfig["OAuthSecret"]
 
-config = configHandler.readConfig()
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+twitter = tweepy.API(auth)
 
 def constructMessage():
-    spots = config["SpitCastAPI"]["Locations"]
     for spot in spots:
+        # strftime formats the date into YYYYMMDD for the URL parameter
         urlToRequest = SPITCAST_URL + spot + URL_PARAMETER + tomorrow.strftime('%Y%m%d')
         urlResponse = urllib.urlopen(urlToRequest)
-        responseData = json.loads(request.read())
+        responseData = json.loads(urlResponse.read())
 
 def postTweet(message):
-    twitter = None
     if twitter is None:
         twitterConfig = config["TwitterAPI"]
         consumerKey = twitterConfig["ConsumerKey"]
