0cc83de0cd412ba9254f0083b18f45a2948921b1
Erick Velez <erickvelez7@gmail.com>
2018-01-11 20:51:28 -0800

    Consolidated Twitter Auth Twitter is now set in a function. Travis CI also added

--- apiHandler.py
+++ apiHandler.py
@@ -1,42 +1,34 @@
 import urllib, json, tweepy, configHandler, datetime
 
 config = configHandler.readConfig()
-spots = config["SpitCastAPI"]["Locations"]
+# spots are Spitcast's ID for locations
+spotArray = config["SpitCastAPI"]["Locations"]
 today = datetime.datetime.today()
 tomorrow = today + datetime.timedelta(days=1)
 
 SPITCAST_URL = "http://api.spitcast.com/api/spot/forecast/"
 URL_PARAMETER = "/?dval="
-twitterConfig = config["TwitterAPI"]
-consumerKey = twitterConfig["ConsumerKey"]
-consumerSecret = twitterConfig["ConsumerSecret"]
-accessToken = twitterConfig["OAuthToken"]
-accessTokenSecret = twitterConfig["OAuthSecret"]
-
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)
-
-twitter = tweepy.API(auth)
 
 def constructMessage():
-    for spot in spots:
+    for spot in spotArray:
         # strftime formats the date into YYYYMMDD for the URL parameter
         urlToRequest = SPITCAST_URL + spot + URL_PARAMETER + tomorrow.strftime('%Y%m%d')
         urlResponse = urllib.urlopen(urlToRequest)
         responseData = json.loads(urlResponse.read())
 
-def postTweet(message):
+def postTweet():
     if twitter is None:
-        twitterConfig = config["TwitterAPI"]
-        consumerKey = twitterConfig["ConsumerKey"]
-        consumerSecret = twitterConfig["ConsumerSecret"]
+        setTwitterAuth()
+    print twitter
 
-        accessToken = twitterConfig["OAuthToken"]
-        accessTokenSecret = twitterConfig["OAuthSecret"]
+def setTwitterAuth():
+    twitterConfig = config["TwitterAPI"]
+    consumerKey = twitterConfig["ConsumerKey"]
+    consumerSecret = twitterConfig["ConsumerSecret"]
+    accessToken = twitterConfig["OAuthToken"]
+    accessTokenSecret = twitterConfig["OAuthSecret"]
 
-        auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-        auth.set_access_token(accessToken, accessTokenSecret)
+    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessToken, accessTokenSecret)
 
-        twitter = tweepy.API(auth)
-
-    twitter.update_status(message)
+    twitter = tweepy.API(auth)
