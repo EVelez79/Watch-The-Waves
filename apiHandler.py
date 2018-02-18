c833adcc057a5bd918292a0ab5d5bcf9202ea4a7
Erick Velez <erickvelez7@gmail.com>
2018-02-18 08:24:59 -0800

    Observe forecast changes Logging will allow to observe whether or not the forecasts are static, which will determine changes

--- apiHandler.py
+++ apiHandler.py
@@ -1,6 +1,18 @@
 import urllib, json, tweepy, configHandler, datetime
 
 config = configHandler.readConfig()
+
+twitterConfig = config["TwitterAPI"]
+consumerKey = twitterConfig["ConsumerKey"]
+consumerSecret = twitterConfig["ConsumerSecret"]
+accessToken = twitterConfig["OAuthToken"]
+accessTokenSecret = twitterConfig["OAuthSecret"]
+
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+twitter = tweepy.API(auth)
+
 # spots are Spitcast's ID for locations
 spotArray = config["SpitCastAPI"]["Locations"]
 today = datetime.datetime.today()
@@ -8,26 +20,39 @@ tomorrow = today + datetime.timedelta(days=1)
 
 SPITCAST_URL = "http://api.spitcast.com/api/spot/forecast/"
 URL_PARAMETER = "/?dval="
+TWEET_INTRO = "Tomorrow's surf forecast for "
+AT = " at "
+WIND = "Wind condition: "
+WAVE = "Wave height: "
+
 
 def constructMessage():
+    for spot in spotArray:
+        responseData = getForecast
+
+        spotName = responseData[12]["spot_name"]
+        forecastTime = responseData[12]["hour"]
+        windCondition = responseData[12]["shape_detail"]["wind"]
+        waveHeight = responseData[12]["size"]
+
+        tweet = TWEET_INTRO + spotName + AT + str(forecastTime) + ". " + WIND + windCondition + ", " + WAVE  + str(waveHeight) + "ft"
+
+        postTweet(tweet)
+
+        time.sleep(480)
+
+
+def getForecast():
+    responseData = list()
     for spot in spotArray:
         # strftime formats the date into YYYYMMDD for the URL parameter
         urlToRequest = SPITCAST_URL + spot + URL_PARAMETER + tomorrow.strftime('%Y%m%d')
         urlResponse = urllib.urlopen(urlToRequest)
-        responseData = json.loads(urlResponse.read())
+        rawData = json.loads(urlResponse.read())
+        responseData.append(rawData)
+    return responseData
+
 
 def postTweet(message):
-    if twitter is None:
-        setTwitterAuth()
 
-def setTwitterAuth():
-    twitterConfig = config["TwitterAPI"]
-    consumerKey = twitterConfig["ConsumerKey"]
-    consumerSecret = twitterConfig["ConsumerSecret"]
-    accessToken = twitterConfig["OAuthToken"]
-    accessTokenSecret = twitterConfig["OAuthSecret"]
-
-    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-    auth.set_access_token(accessToken, accessTokenSecret)
-
-    twitter = tweepy.API(auth)
+    twitter.update_status(message)
--- tweetBot.py
+++ tweetBot.py
@@ -1,3 +1,20 @@
-import apiHandler
+import apiHandler, time, logging
 
-apiHandler.constructMessage()
+logging.basicConfig(filename="api.log",filemode="a",LEVEL=logging.INFO)
+
+counter = 0
+
+logger = open("apiLog-{}.log".format(time.time()),"w")
+
+splitter = "(!@!)"
+
+while counter <= 1000 :
+    #apiHandler.constructMessage()
+    apiData = str(apiHandler.getForecast())
+    logger.write(apiData + "\n\n" + splitter + str(time.time()) + "\n\n")
+    print "Api Call #{}\n".format(counter)
+    counter += 1
+    time.sleep(1800)#30 Mins
+
+logger.close()
+print "Done"
