9e106e08e54fe9339fd7c9a8432dc0f3280386ab
root <root@DESKTOP-4RJKOLE.localdomain>
2018-03-03 20:21:09 -0800

    Put twitter api handling into its own class

--- apiHandler.py
+++ apiHandler.py
@@ -9,25 +9,32 @@ tomorrow = today + datetime.timedelta(days=1)
 SPITCAST_URL = "http://api.spitcast.com/api/spot/forecast/"
 URL_PARAMETER = "/?dval="
 
-def constructMessage():
-    for spot in spotArray:
-        # strftime formats the date into YYYYMMDD for the URL parameter
-        urlToRequest = SPITCAST_URL + spot + URL_PARAMETER + tomorrow.strftime('%Y%m%d')
-        urlResponse = urllib.urlopen(urlToRequest)
-        responseData = json.loads(urlResponse.read())
+class Twitter:
+    def __init__(twitterConfig):
+        self.config = twitterConfig
+        self.twitter = self._setupTwitterAuth()
 
-def postTweet(message):
-    if twitter is None:
+
+    def constructMessage():
+        for spot in spotArray:
+            # strftime formats the date into YYYYMMDD for the URL parameter
+            urlToRequest = SPITCAST_URL + spot + URL_PARAMETER + tomorrow.strftime('%Y%m%d')
+            urlResponse = urllib.urlopen(urlToRequest)
+            responseData = json.loads(urlResponse.read())
+
+    #TODO implement the actual
+    def postTweet(message):
         setTwitterAuth()
+        api.update_status(message)
 
-def setTwitterAuth():
-    twitterConfig = config["TwitterAPI"]
-    consumerKey = twitterConfig["ConsumerKey"]
-    consumerSecret = twitterConfig["ConsumerSecret"]
-    accessToken = twitterConfig["OAuthToken"]
-    accessTokenSecret = twitterConfig["OAuthSecret"]
+    def _setupTwitterAuth():
+        twitterConfig = self.config["TwitterAPI"]
+        consumerKey = self.config["ConsumerKey"]
+        consumerSecret = self.config["ConsumerSecret"]
+        accessToken = self.config["OAuthToken"]
+        accessTokenSecret = self.config["OAuthSecret"]
 
-    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-    auth.set_access_token(accessToken, accessTokenSecret)
+        auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+        auth.set_access_token(accessToken, accessTokenSecret)
 
-    twitter = tweepy.API(auth)
+        return tweepy.API(auth)
