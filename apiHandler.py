32f0692f76f96a7579f81e89de95aa6453c4ab15
Stellan Muller-Cohn <stellan125@gmail.com>
2018-04-03 10:23:39 -0700

    implemented OpenWeather API. Fixed SpitCast WaterTemp access

--- apiHandler.py
+++ apiHandler.py
@@ -1,5 +1,4 @@
-import urllib, json, tweepy, datetime
-from weather import Weather
+import urllib2, json, tweepy, datetime
 
 
 #TODO create message format with the api data
@@ -10,21 +9,61 @@ def constructMessage():
         continue
 
 
-Weather = Weather(unit="f")
+def _httpGet(url):
+    urlResponse = urllib2.urlopen(url)
+    read = urlResponse.read()
+    return read
+
+
+class OpenWeather:
+    OPEN_WEATHER_URL = "http://api.openweathermap.org/data/2.5/"
+    KEY_PARAM = "&appid="
+    ZIP_PARAM = "?zip="
+    WEATHER_ENDPOINT = "weather"
+
+
+    def __init__(self, key):
+        self.key = key
+
+
+    def getForecastByZip(self, zip):
+        urlToRequest = self.OPEN_WEATHER_URL + self.WEATHER_ENDPOINT + self.ZIP_PARAM + str(zip) + self.KEY_PARAM + self.key
+        forecast = json.loads(_httpGet(urlToRequest))
+        return forecast
+
 
 class SpitCast:
+    FORECAST_PATH = "/spot/forecast"  # suffix: spot
+    TEMPERATURE_PATH = "/county/water-temperature"  # suffix: countyName
+    SPITCAST_URL = "http://api.spitcast.com/api"
+    SPITCAST_URL_PARAMETER = "/?dval="
+
+
     def __init__(self):
-        # spots are Spitcast's ID for locations
-        self.SPITCAST_URL = "http://api.spitcast.com/api/spot/forecast/"
-        self.SPITCAST_URL_PARAMETER = "/?dval="
+        pass
 
 
-    def callApi(self, spot):
+    #currently all forecasts are only one day ahead
+    #TODO add variable timedelta for the forecast
+    def getForecast(self, spot):
         today = datetime.datetime.today()
         tomorrow = today + datetime.timedelta(days=1)
-        urlToRequest = self.SPITCAST_URL + spot + self.SPITCAST_URL_PARAMETER + tomorrow.strftime('%Y%m%d')
-        urlResponse = urllib.urlopen(urlToRequest)
-        return json.loads(urlResponse.read())
+        urlToRequest = self.SPITCAST_URL + self.FORECAST_PATH + "/" + spot + self.SPITCAST_URL_PARAMETER + tomorrow.strftime('%Y%m%d')
+        urlResponse = json.loads(_httpGet(urlToRequest))
+
+        return urlResponse
+
+
+    #error 500 when requesting for los-angeles
+    def getWaterTemp(self, spot):
+        today = datetime.datetime.today()
+        tomorrow = today + datetime.timedelta(days=1)
+
+        urlToRequest = self.SPITCAST_URL + self.TEMPERATURE_PATH + "/" + spot +"/"
+        u = _httpGet(urlToRequest)
+        urlResponse = json.loads(u)
+
+        return urlResponse
 
 
 class Twitter:
@@ -33,9 +72,12 @@ class Twitter:
         self.twitter = self._setupTwitterAuth()
 
 
-    #TODO implement tweeting
-    def postTweet(self, message):
-        self.twitter.update_status(message)
+    #imagePath is a string
+    def postTweet(self, message, imagePath=None):
+        if imagePath == None:
+            self.twitter.update_status(message)
+        else:
+            self.twitter.update_with_media(imagePath, message)
 
 
     def _setupTwitterAuth(self):
--- tweetBot.py
+++ tweetBot.py
@@ -1,37 +1,12 @@
-import apiHandler, configHandler, datetime, urllib, json
+import apiHandler, datetime, urllib, json
 
-config = configHandler.readConfig()
-spotArray = config["SpitCastAPI"]["Locations"]  # spots are Spitcast's ID for locations
+CONFIG_DIR = "Resources/config.json"
+DATABASE_DIR = "/Data/"
+
+config = json.loads(open(CONFIG_DIR, "r").read())
+
+spotArray = config["Locations"]
 today = datetime.datetime.today()
 tomorrow = today + datetime.timedelta(days=1)
 
-jsonDict = dict()
-
-SPITCAST_URL = "http://api.spitcast.com/api"
-FORECAST_PATH = "/spot/forecast/"  # suffix: spot
-TEMPERATURE_PATH = "/county/water-temperature/"  # suffix: countyName
-
-twitter = apiHandler.set_twitter_auth(config)
-
-def get_data(spot, urlToRequest):
-    urlResponse = urllib.urlopen(urlToRequest)
-    return json.loads(urlResponse.read())
-
-
-def write_json(responseData, fileToWriteTo):
-    jsonFile = open("Resources/JSON Files/forecast.txt", "w+")
-    json.dump(responseData, jsonFile)
-    jsonFile.close()
-
-
-def get_forecast():
-    for spot in spotArray:
-        urlToRequest = SPITCAST_URL + FORECAST_PATH + spot
-        return get_data(spot, urlToRequest)
-
-
-def main():
-    responseData = get_forecast()
-    write_json(responseData)
-
-main()
+twitter = apiHandler.Twitter(config["TwitterAPI"])
